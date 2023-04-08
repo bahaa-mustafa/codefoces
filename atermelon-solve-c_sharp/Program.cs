cee838784f8357e5da13f9f965d1939ace6808f9
bahaa-mustafa <bahaamustafabadr@gmail.com>
2023-04-08 23:21:32 +0200

    solving to watermelon problem

---
+++ atermelon-solve-c_sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atermelon_solve_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int w= Convert.ToInt16(Console.ReadLine());
+
+            if (w > 2)
+            {
+                if(w%2==0)
+                {
+                    Console.WriteLine("YES");
+                }
+
+                else
+                    Console.WriteLine("NO");
+
+
+            }
+            else if(w==0 || w==1 || w==2 )
+            { Console.WriteLine("NO"); }
+
+            else
+                Console.WriteLine("enter a right value please");
+
+            Console.ReadLine();
+
+        }
+    }
+}
