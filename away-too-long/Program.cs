c4aa0bf798e174198075381abf330d6d41ca84ab
bahaa-mustafa <bahaamustafabadr@gmail.com>
2023-04-09 14:32:27 +0200

    solving to way too long, and a team train.

---
+++ away-too-long/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace away_too_long_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] words = new string[n];
+            for(int i=0;i<n;i++)
+            {
+                words[i] = Console.ReadLine();
+
+            }
+            for(int i=0; i<n; i++)
+            {
+                if (words[i].Length > 10)
+                {
+                    Console.WriteLine($"{words[i].First()}{words[i].Length - 2}{words[i].Last()} ");
+                }
+
+                else
+                    Console.WriteLine(words[i]);
+            }
+           
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ team/ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //DisplayResult(RemoveWhiteSpace(yourName));
+            //Console.Write("enter num of loops do you want: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+           // string[] answers = new string[n];
+            int trueans = 0;
+            for(int i=0; i<n; i++)
+            {
+                //Console.Write("Enter your name: ");
+                string yourName = Console.ReadLine();
+
+                //Console.WriteLine($"hello {yourName}!");
+                yourName = yourName.Replace(" ", "");
+                int value1 = 0;
+
+
+                foreach (char o in yourName)
+                {
+
+                   // Console.WriteLine(o);
+                    int newnum = int.Parse(o.ToString());
+                    //Console.WriteLine($"by int value: {newnum}.");
+                    if (newnum == 1)
+                        value1++;
+
+
+                }
+
+                //Console.WriteLine("num of 1: " + value1);
+                if(value1 >= 2)
+                {
+                    //Console.WriteLine("yes");
+                    trueans++;
+                    //answers[i] = "yes";
+                }
+                //else
+                //{
+                //    //Console.WriteLine("no");
+                //    answers[i] = "no";
+                //}
+                //Console.WriteLine(" ------------------ ");
+            }
+            //foreach(string ans in answers)
+            //{
+            //    //Console.WriteLine($" the total answer:");
+            //    Console.WriteLine(ans);
+
+            //}
+            Console.WriteLine(trueans);
+            // Console.WriteLine(" ------------------ ");
+
+
+
+            // Console.ReadLine();
+
+        }
+        //private static string RemoveWhiteSpace(string message)
+        //{
+        //    return message.Replace(" ", "");
+        //}
+
+        //private static void DisplayResult(string message)
+        //{
+        //    Console.WriteLine($"Your modified string is: {message}");
+        //}
+    }
+}
