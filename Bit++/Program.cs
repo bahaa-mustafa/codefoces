691bb4f55a80e516e0b32b9933327ee53e3c9fbe
bahaa-mustafa <bahaamustafabadr@gmail.com>
2023-04-23 18:24:24 +0200

    solveing for Bit++ problem #800

---
+++ Bit++/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 0;
+            string n;
+            int y = Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<y; i++)
+            {
+                n = Console.ReadLine();
+                if(n =="X++" ||  n =="++X")
+                    X++;
+                else
+                    X--;
+            }
+            Console.WriteLine(X);
+            Console.ReadLine();
+        }
+    }
+}
